96c72144dd42edafe113f5885892c19976f2a6fb
15779506819 <944780299@qq.com>
2020-01-13 14:34:11 +0800

    动态列表

--- demo01/lib/main.dart
+++ demo01/lib/main.dart
@@ -1,78 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MyApp(items: List<String>.generate(100, (index) => "Item$index")));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  const MyApp({Key key, @required this.items}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //列表组件
-      title: 'imooc_flutter_demo',
-      home: Scaffold(
-          appBar: AppBar(title: new Text('listView Widget')),
-          // body: new ListView(
-          //   children: <Widget>[
-          //     // new ListTile(
-          //     //   leading: new Icon(Icons.account_circle),
-          //     //   title: new Text('account_circle'),
-          //     // ),
-          //     // new ListTile(
-          //     //   leading: new Icon(Icons.border_inner),
-          //     //   title: new Text('border_inner'),
-          //     // ),
-          //     // new ListTile(
-          //     //   leading: new Icon(Icons.add_box),
-          //     //   title: new Text('add_box'),
-          //     // )
-          //     new Image.network(
-          //         'https://img3.mukewang.com/5c18cf540001ac8206000338-360-202.jpg'),
-          //     new Image.network(
-          //         'https://img2.mukewang.com/5b6947970001d34d06000338-360-202.jpg'),
-          //     new Image.network(
-          //         'https://img4.mukewang.com/5950ad6f0001903f06000338-360-202.jpg'),
-          //     new Image.network(
-          //         'https://img.mukewang.com/5ae3e5b80001818c06000338-360-202.jpg')
-          //   ],
-          // )
-
-
-
-        body: Center(
-          child: new Container(
-            height: 300.0,
-            child: MyList(),
+        title: 'Hello Flutter',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("ListView Widget"),
+            backgroundColor: Colors.indigoAccent,
+            //标题居中
+            centerTitle: true,
           ),
-        ),
-    )
-    );
-  }
-}
-
-
-//横向列表组件
-class MyList extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                new Container(
-                  width: 200.0,
-                  color: Colors.cyan,
-                ),
-                new Container(
-                  width: 200.0,
-                  color: Colors.red,
-                ),
-                new Container(
-                  width: 200.0,
-                  color: Colors.grey,
-                ),
-                new Container(
-                  width: 200.0,
-                  color: Colors.purple,
-                )
-              ],
-            );
+          body: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${items[index]}"),
+                );
+              }),
+        ));
   }
 }
