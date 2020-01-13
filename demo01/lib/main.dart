5b9b4e1cf08f85f9f90b43a5c916bd014d8f7ecf
15779506819 <944780299@qq.com>
2020-01-13 11:38:27 +0800

    横向列表组件

--- demo01/lib/main.dart
+++ demo01/lib/main.dart
@@ -4,36 +4,75 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext ontent) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       //列表组件
       title: 'imooc_flutter_demo',
       home: Scaffold(
           appBar: AppBar(title: new Text('listView Widget')),
-          body: new ListView(
-            children: <Widget>[
-              // new ListTile(
-              //   leading: new Icon(Icons.account_circle),
-              //   title: new Text('account_circle'),
-              // ),
-              // new ListTile(
-              //   leading: new Icon(Icons.border_inner),
-              //   title: new Text('border_inner'),
-              // ),
-              // new ListTile(
-              //   leading: new Icon(Icons.add_box),
-              //   title: new Text('add_box'),
-              // )
-              new Image.network(
-                  'https://img3.mukewang.com/5c18cf540001ac8206000338-360-202.jpg'),
-              new Image.network(
-                  'https://img2.mukewang.com/5b6947970001d34d06000338-360-202.jpg'),
-              new Image.network(
-                  'https://img4.mukewang.com/5950ad6f0001903f06000338-360-202.jpg'),
-              new Image.network(
-                  'https://img.mukewang.com/5ae3e5b80001818c06000338-360-202.jpg')
-            ],
-          )),
+          // body: new ListView(
+          //   children: <Widget>[
+          //     // new ListTile(
+          //     //   leading: new Icon(Icons.account_circle),
+          //     //   title: new Text('account_circle'),
+          //     // ),
+          //     // new ListTile(
+          //     //   leading: new Icon(Icons.border_inner),
+          //     //   title: new Text('border_inner'),
+          //     // ),
+          //     // new ListTile(
+          //     //   leading: new Icon(Icons.add_box),
+          //     //   title: new Text('add_box'),
+          //     // )
+          //     new Image.network(
+          //         'https://img3.mukewang.com/5c18cf540001ac8206000338-360-202.jpg'),
+          //     new Image.network(
+          //         'https://img2.mukewang.com/5b6947970001d34d06000338-360-202.jpg'),
+          //     new Image.network(
+          //         'https://img4.mukewang.com/5950ad6f0001903f06000338-360-202.jpg'),
+          //     new Image.network(
+          //         'https://img.mukewang.com/5ae3e5b80001818c06000338-360-202.jpg')
+          //   ],
+          // )
+
+
+
+        body: Center(
+          child: new Container(
+            height: 300.0,
+            child: MyList(),
+          ),
+        ),
+    )
     );
+  }
+}
+
+
+//横向列表组件
+class MyList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                new Container(
+                  width: 200.0,
+                  color: Colors.cyan,
+                ),
+                new Container(
+                  width: 200.0,
+                  color: Colors.red,
+                ),
+                new Container(
+                  width: 200.0,
+                  color: Colors.grey,
+                ),
+                new Container(
+                  width: 200.0,
+                  color: Colors.purple,
+                )
+              ],
+            );
   }
 }
