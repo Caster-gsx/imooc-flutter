a48ceff4c877f613f9db5bbf46a6afbad1f60d92
15779506819 <944780299@qq.com>
2020-01-14 17:06:39 +0800

    if语句

---
+++ dart/statement_if.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  double score = 90.8;
+  print(score);
+
+  if(score >= 90) {
+    print('优秀');
+  }else if(score >= 60) {
+    print('良好');
+  }else {
+    print('加油哇');
+  }
+}
