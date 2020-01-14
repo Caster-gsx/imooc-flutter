6046c955b80c6edf0dc183ad5b2d4512de6a0fad
15779506819 <944780299@qq.com>
2020-01-14 16:09:09 +0800

    赋值运算符_asignment

---
+++ dart/operator_asignment.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int a = 10;
+
+  // ??=  如果b为空，则将值分配给b；否则，b保持不变
+  int b;
+  b ??= 20;
+  print(b);
+
+  int c = 5;
+  c ??= 20;
+  print(c);
+
+  // +=  -=  *=  /=  !/=   %=
+}
